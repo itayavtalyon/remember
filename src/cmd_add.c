8cbbb98bacfc8c3faa8c510e00c2a5d726238cc8
Itay Avtalyon <itay@imoogi.com>
2026-07-26 12:39:07 +0300

    Fix Linux IWYU: include stdbool.h in command TUs.
    bool comes through commands.h transitively on some toolchains; include-what-you-use on Ubuntu requires a direct include.

--- src/cmd_add.c
+++ src/cmd_add.c
@@ -7,6 +7,7 @@
 #include "store.h"
 #include "util.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/cmd_locator.c
+++ src/cmd_locator.c
@@ -7,6 +7,7 @@
 #include "store.h"
 
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
